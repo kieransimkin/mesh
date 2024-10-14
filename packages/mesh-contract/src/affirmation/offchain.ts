f45860d7652f5678f19797e6fb8b3b42698a5f25
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-10-14 07:49:13 +0100

    I need to be able to generate a stake key signature from the embedded wallet, as it does not honour the required signers field.

--- packages/mesh-contract/src/affirmation/offchain.ts
+++ packages/mesh-contract/src/affirmation/offchain.ts
@@ -4,41 +4,115 @@ import {
   ConStr0,
   deserializeAddress,
   Integer,
+  resolveScriptHash,
   mConStr0,
+  mConStr1,
   serializePlutusScript,
   stringToHex,
   UTxO,
 } from "@meshsdk/core";
-import { applyParamsToScript } from "@meshsdk/core-csl";
+import { applyParamsToScript , applyCborEncoding} from "@meshsdk/core-csl";
+import { Address,Credential,AddressType } from "@meshsdk/core-cst"
 
 import { MeshTxInitiator, MeshTxInitiatorInput } from "../common";
-import blueprint from "./aiken-workspace/plutus.json";
+import blueprint from "./plutus.json";
 
-export type HelloWorldDatum = ConStr0<
-  [Integer, BuiltinByteString, BuiltinByteString]
+export type AffirmationDatum = ConStr0<
+  [Integer, BuiltinByteString]
 >;
 
-export const MeshHelloWorldBlueprint = blueprint;
-
-export class MeshHelloWorldContract extends MeshTxInitiator {
-  scriptCbor = applyParamsToScript(blueprint.validators[0]!.compiledCode, []);
+export const MeshAffirmationBlueprint = blueprint;
+type scriptInfo ={ 
+  address: string, policyId?: string, scriptCbor?: string
+}
+export class MeshAffirmationContract extends MeshTxInitiator {
+  
 
   constructor(inputs: MeshTxInitiatorInput) {
     super(inputs);
+    this.languageVersion = 'V3';
   }
 
-  getScript = () => {
-    const { address } = serializePlutusScript(
-      { code: this.scriptCbor, version: "V2" },
-      undefined,
+
+  getScript = (type: 'Mint' | 'Spend', beneficiaryKeyHash: string ) => {
+    const scriptCbor:string=applyCborEncoding(blueprint?.validators[type=='Mint'?0:1]?.compiledCode || '');
+    const policyId:string = resolveScriptHash(scriptCbor, "V3");
+    const script:scriptInfo = serializePlutusScript(
+      { code: scriptCbor, version: "V3" },
+      beneficiaryKeyHash,
       this.networkId,
     );
-    return {
-      scriptAddr: address,
-    };
+    script.policyId = policyId;
+    script.scriptCbor = scriptCbor;
+    return script;
   };
+  affirm = async (beneficiary: Credential): Promise<string> => {
+    const { utxos, walletAddress, collateral } = await this.getWalletInfoForTx();
+    
+    const stakeAddrBech = walletAddress || "";
+    const stakeAddr = Address.fromBech32(stakeAddrBech);
+    const stakeAddrProps = stakeAddr.getProps();
+    const stakeHash = stakeAddrProps.delegationPart?.hash || "";
+    const walletHash = stakeAddrProps.paymentPart?.hash || "";
+    const mintingScript = this.getScript("Mint", stakeHash);
+    const spendingScript = this.getScript("Spend", stakeHash);
+    const targetAddress = mintingScript.address;
+    
 
-  lockAsset = async (assets: Asset[]): Promise<string> => {
+    
+    
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    const assets:Asset[] = [];
+    
+    assets.push({unit: mintingScript.policyId+stakeHash, quantity:'1'})
+    console.log(mintingScript.policyId);
+    console.log(stakeHash);
+    await this.mesh
+    .mintPlutusScript(this.languageVersion)
+    .mint("1", mintingScript.policyId || '', stakeHash)
+    .txOutInlineDatumValue(mConStr0([stakeHash]))
+    .txOut(targetAddress, assets).txOutDatumHashValue(mConStr0([stakeHash]))
+    
+    .mintingScript(mintingScript.scriptCbor || '')
+    .mintRedeemerValue(mConStr1([]))
+    
+    .txInCollateral(
+      collateral.input.txHash,
+      collateral.input.outputIndex,
+      collateral.output.amount,
+      collateral.output.address,
+    )
+    .changeAddress(walletAddress)
+    .selectUtxosFrom(utxos) 
+    .requiredSignerHash(stakeHash)
+    .requiredSignerHash(walletHash)
+    .complete();
+    return this.mesh.txHex;
+    //const spendingScript:string = applyCborEncoding(blueprint?.validators[1]?.compiledCode || '');
+    //const targetAddress = new Address({type:AddressType.BasePaymentScriptStakeKey,networkId: this.networkId, paymentPart: Credential})
+    /*
+    export declare type AddressProps = {
+      type: AddressType;
+      networkId?: NetworkId;
+      pointer?: Pointer;
+      paymentPart?: Credential;
+      delegationPart?: Credential;
+      byronAddressContent?: ByronAddressContent;
+  };
+  */
+    console.log(stakeAddrProps);
+
+    return 'foo';
+    /*
     const { utxos, walletAddress } = await this.getWalletInfoForTx();
     const { scriptAddr } = this.getScript();
     const signerHash = deserializeAddress(walletAddress).pubKeyHash;
@@ -50,9 +124,12 @@ export class MeshHelloWorldContract extends MeshTxInitiator {
       .selectUtxosFrom(utxos)
       .complete();
     return this.mesh.txHex;
+    */
   };
 
-  unlockAsset = async (scriptUtxo: UTxO, message: string): Promise<string> => {
+  revoke = async (scriptUtxo: UTxO, message: string): Promise<string> => {
+    return 'foo';
+    /*
     const { utxos, walletAddress, collateral } =
       await this.getWalletInfoForTx();
     const signerHash = deserializeAddress(walletAddress).pubKeyHash;
@@ -80,9 +157,7 @@ export class MeshHelloWorldContract extends MeshTxInitiator {
       .complete();
 
     return this.mesh.txHex;
+    */
   };
 
-  getUtxoByTxHash = async (txHash: string): Promise<UTxO | undefined> => {
-    return await this._getUtxoByTxHash(txHash, this.scriptCbor);
-  };
 }
